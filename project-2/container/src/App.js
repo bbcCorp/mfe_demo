2d8ea652261484482b006cd44c315f4e22528540
Bedabrata Chatterjee <bedabrata.chatterjee@gmail.com>
2021-08-04 15:28:37 +0530

    Fixed CSS class name collisions

--- project-2/container/src/App.js
+++ project-2/container/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Header from './components/Header';
 import MarketingMfeAppWrapper from './components/MarketingMfeAppWrapper';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'cnt'
+});
+
 export default () => {
 
     return (
+
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingMfeAppWrapper />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingMfeAppWrapper />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
+
+
     )
 };
