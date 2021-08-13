44c5c92f002f6ec9ad8e6157be5d72e8a6f2acba
Bedabrata Chatterjee <bedabrata.chatterjee@gmail.com>
2021-08-13 22:24:14 +0530

    Added lazy loading of child apps

--- project-2/container/src/App.js
+++ project-2/container/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Header from './components/Header';
+import ProgressBar from './components/ProgressBar';
+// // This will cause eager loading of components
+// import MarketingMfeAppWrapper from './components/MarketingMfeAppWrapper';
+// import AuthMfeAppWrapper from './components/AuthMfeAppWrapper';
 
-import MarketingMfeAppWrapper from './components/MarketingMfeAppWrapper';
-import AuthMfeAppWrapper from './components/AuthMfeAppWrapper';
+const LazyLoadMarketingApp = lazy(() => import('./components/MarketingMfeAppWrapper'));
+const LazyLoadAuthApp = lazy(() => import('./components/AuthMfeAppWrapper'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'cnt'
-}); 
+});
 
 export default () => {
 
@@ -18,10 +22,12 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
-                    <Switch>
-                        <Route path="/auth" component={AuthMfeAppWrapper} />
-                        <Route path="/" component={MarketingMfeAppWrapper} />
-                    </Switch>
+                    <Suspense fallback={<ProgressBar />} >
+                        <Switch>
+                            <Route path="/auth" component={LazyLoadAuthApp} />
+                            <Route path="/" component={LazyLoadMarketingApp} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
